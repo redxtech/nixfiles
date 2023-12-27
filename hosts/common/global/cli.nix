1f53eba93b494398de0f81d99a3aba56603066c8
Gabe Dunn <gabe@sent.at>
2023-12-27 12:49:02 -0700

    add some nix aliases

--- hosts/common/global/cli.nix
+++ hosts/common/global/cli.nix
@@ -1,7 +1,28 @@
 { config, pkgs, ... }:
 
 {
-  programs.zsh = { enable = true; };
+  programs.zsh = {
+    enable = true;
+
+    shellAliases = {
+      n = "nix";
+      np = "nix-shell -p";
+      nd = "nix develop -c $SHELL";
+      ns = "nix shell";
+      nsn = "nix shell nixpkgs#";
+      nb = "nix build";
+      nbn = "nix build nixpkgs#";
+      nf = "nix flake";
+
+      nr = "nixos-rebuild --flake .";
+      nrs = "nixos-rebuild --flake . switch";
+      snr = "sudo nixos-rebuild --flake .";
+      snrs = "sudo nixos-rebuild --flake . switch";
+      hm = "home-manager --flake .";
+      hms = "home-manager --flake . switch";
+      hmsb = "home-manager --flake . switch -b backup";
+    };
+  };
 
   environment.systemPackages = with pkgs; [
     atool
