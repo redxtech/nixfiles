a424f2a77214efee1c21229ae66bb1cccb7d344f
Gabe Dunn <gabe@sent.at>
2023-12-27 15:20:29 -0700

    add per-device sops config

--- hosts/common/global/openssh.nix
+++ hosts/common/global/openssh.nix
@@ -17,10 +17,17 @@ in {
       GatewayPorts = "clientspecified";
     };
 
-    hostKeys = [{
-      path = "/etc/ssh/ssh_host_ed25519_key";
-      type = "ed25519";
-    }];
+    hostKeys = [
+      {
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+      {
+        openSSHFormat = true;
+        path = "/etc/ssh/ssh_host_rsa_key";
+        type = "rsa";
+      }
+    ];
   };
 
   programs.ssh = {
