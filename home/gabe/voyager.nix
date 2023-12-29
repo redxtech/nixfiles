5c7233d5ce250f5e2043aebac5dad66b4e6536ba
Gabe Dunn <gabe@sent.at>
2023-12-29 13:38:10 -0700

    add autorandr, dunst, picom, and sxhkd configs

--- home/gabe/voyager.nix
+++ home/gabe/voyager.nix
@@ -18,4 +18,21 @@
     height = 1080;
     primary = true;
   }];
+
+  # desktop layout
+  xsession.windowManager.bspwm = {
+    monitors = { "eDP-1" = [ "shell" "www" "chat" "music" "files" "video" ]; };
+  };
+
+  xdg.configFile."wireplumber/main.lua.d/51-alsa-rename.lua".text = ''
+    table.insert(alsa_monitor.rules, {
+      matches = { { { "node.name", "matches", "alsa_output.pci-0000_00_1f.3.*" } } },
+      apply_properties = { ["node.description"] = "Speakers" },
+    })
+
+    table.insert(alsa_monitor.rules, {
+      matches = { { { "node.name", "matches", "alsa_output.usb-AudioQuest_AudioQuest_DragonFly_Red*" } } },
+      apply_properties = { ["node.description"] = "DAC" },
+    })
+  '';
 }
