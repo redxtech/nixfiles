29da68c8cb3cafec8cf2dc254969694d3ba43d91
Gabe Dunn <gabe@sent.at>
2023-12-29 18:20:19 -0700

    add youtube secret path

--- home/gabe/global/sops.nix
+++ home/gabe/global/sops.nix
@@ -1,8 +1,14 @@
 { inputs, lib, config, ... }:
 
 {
+  imports = [ inputs.sops-nix.homeManagerModules.sops ];
+
   sops = {
     defaultSopsFile = ../secrets.yaml;
     age.sshKeyPaths = [ "$HOME/.ssh/id_ed25519" ];
+
+    secrets = {
+      youtube = { path = "${config.xdg.configHome}/rofi/youtube.txt"; };
+    };
   };
 }
