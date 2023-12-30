7d4ddcc0a7de99e30decff2c36b136ac200905a1
Gabe Dunn <gabe@sent.at>
2023-12-30 01:42:05 -0700

    add per-device variables

---
+++ home/gabe/features/desktop/bspwm/per-device/bastion.nix
@@ -0,0 +1,52 @@
+# place to set per-device variables to keep the configuration DRY
+{ lib, config, pkgs, ... }:
+
+# TODO: convert to module
+let
+  deviceVar = lib.mkOptionType {
+    name = "device-variable";
+    description = "variable that represents a per-device variable";
+  };
+
+  mkDeviceVarOption = name: default:
+    (lib.mkOption {
+      type = deviceVar;
+      description = "The device-specific variable for '${name}'.";
+      inherit default;
+    });
+in {
+  options.device-vars = builtins.mapAttrs mkDeviceVarOption rec {
+    # device os
+    isNixOS = true;
+
+    # primary monitor
+    monitor = "DP-1";
+
+    # netowrk options
+    networkType = "wired";
+    networkInterface = "enp39s0";
+
+    # bar options
+    barRightModules = [
+      "weather"
+      "margin"
+      # "kdeconnect"
+      # "margin"
+      "pipewire"
+      "margin"
+      "memory"
+      "margin"
+      "temperature"
+      "margin"
+      "cpu"
+      "margin"
+      "network"
+      "margin"
+      "date"
+      "margin"
+      "dnd"
+    ];
+    hwmonPath = "/sys/devices/pci0000:00/0000:00:18.3/hwmon/hwmon3/temp3_input";
+  };
+}
+
---
+++ home/gabe/features/desktop/bspwm/per-device/voyager.nix
@@ -0,0 +1,57 @@
+# place to set per-device variables to keep the configuration DRY
+{ lib, config, pkgs, ... }:
+
+# TODO: convert to module
+let
+  deviceVar = lib.mkOptionType {
+    name = "device-variable";
+    description = "variable that represents a per-device variable";
+  };
+
+  mkDeviceVarOption = name: default:
+    (lib.mkOption {
+      type = deviceVar;
+      description = "The device-specific variable for '${name}'.";
+      inherit default;
+    });
+in {
+  options.device-vars = builtins.mapAttrs mkDeviceVarOption rec {
+    # device os
+    isNixOS = true;
+
+    # primary monitor
+    monitor = "eDP-1";
+
+    # netowrk options
+    networkType = "wireless";
+    networkInterface = "wlp59s0";
+
+    # bar options
+    barRightModules = [
+      "margin"
+      "weather"
+      "margin"
+      "backlight"
+      "margin"
+      # "kdeconnect"
+      # "margin"
+      "pipewire"
+      "margin"
+      "memory"
+      "margin"
+      "temperature"
+      "margin"
+      "cpu"
+      "margin"
+      "network"
+      "margin"
+      "battery"
+      "margin"
+      "date"
+      "margin"
+      "dnd"
+    ];
+    hwmonPath = "/sys/devices/platform/coretemp.0/hwmon/hwmon6/temp1_input";
+  };
+}
+
