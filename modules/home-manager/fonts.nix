505333cfdb8ffd24db9a40fc03b53cf87e761829
Gabe Dunn <gabe@sent.at>
2023-12-30 20:43:34 -0700

    add symbols and extrafonts to hm font module

--- modules/home-manager/fonts.nix
+++ modules/home-manager/fonts.nix
@@ -1,32 +1,40 @@
 { lib, config, ... }:
 
 let
-  mkFontOption = kind: {
-    family = lib.mkOption {
-      type = lib.types.str;
-      default = null;
-      description = "Family name for ${kind} font profile";
-      example = "Fira Code";
+  mkFontOption = kind:
+    with lib; {
+      family = mkOption {
+        type = types.str;
+        default = null;
+        description = "Family name for ${kind} font profile";
+        example = "Fira Code";
+      };
+      package = mkOption {
+        type = types.package;
+        default = null;
+        description = "Package for ${kind} font profile";
+        example = "pkgs.fira-code";
+      };
     };
-    package = lib.mkOption {
-      type = lib.types.package;
-      default = null;
-      description = "Package for ${kind} font profile";
+  cfg = config.fontProfiles;
+in with lib; {
+  options.fontProfiles = {
+    enable = mkEnableOption "Whether to enable font profiles";
+    monospace = mkFontOption "monospace";
+    regular = mkFontOption "regular";
+    symbols = mkFontOption "symbols";
+    extraFonts = mkOption {
+      type = types.listOf types.package;
+      default = [ ];
+      description = "Extra font packages to install";
       example = "pkgs.fira-code";
     };
   };
-  cfg = config.fontProfiles;
-in
-{
-  options.fontProfiles = {
-    enable = lib.mkEnableOption "Whether to enable font profiles";
-    monospace = mkFontOption "monospace";
-    regular = mkFontOption "regular";
-  };
 
-
-  config = lib.mkIf cfg.enable {
+  config = mkIf cfg.enable {
     fonts.fontconfig.enable = true;
-    home.packages = [ cfg.monospace.package cfg.regular.package ];
+    home.packages =
+      [ cfg.monospace.package cfg.regular.package cfg.symbols.package ]
+      ++ cfg.extraFonts;
   };
 }
