4a2b139b2b3f37c011addf4bf9ff8f71ee9fbee4
Gabe Dunn <gabe@sent.at>
2024-01-02 20:29:22 -0700

    add zinit override to include docs

--- overlays/default.nix
+++ overlays/default.nix
@@ -55,6 +55,24 @@ in {
       # enableWidevine = true; # TODO: vivaldi crashes when this is enabled, need to fix
     };
 
+    zinit = prev.zinit.overrideAttrs (oldAttrs: {
+      installPhase = ''
+        outdir="$out/share/$pname"
+        cd "$src"
+        ls -al doc
+
+        # Zplugin's source files
+        install -dm0755 "$outdir"
+        # Installing backward compatibility layer
+        install -m0644 zinit{,-side,-install,-autoload}.zsh "$outdir"
+        install -m0755 share/git-process-output.zsh "$outdir"
+        mkdir -p "$outdir/doc"
+        install doc/zinit.1 "$outdir/doc/zinit.1"
+
+        # Zplugin autocompletion
+        installShellCompletion --zsh _zinit
+      '';
+    });
   };
 
   neovim-nightly = inputs.neovim-nightly-overlay.overlay;
