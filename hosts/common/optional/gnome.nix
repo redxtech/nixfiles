7f87a93a3552e46f22f7fba57ae416eee69a9551
Gabe Dunn <gabe@sent.at>
2024-01-03 21:28:47 -0700

    update optional gnome module

--- hosts/common/optional/gnome.nix
+++ hosts/common/optional/gnome.nix
@@ -3,22 +3,37 @@
 {
   services = {
     xserver = {
+      enable = true;
+      layout = "us";
+
       desktopManager.gnome = { enable = true; };
+
       displayManager.gdm = {
         enable = true;
         autoSuspend = false;
       };
+
+      libinput = {
+        enable = true;
+        touchpad = {
+          naturalScrolling = true;
+          # disableWhileTyping = true;
+        };
+      };
     };
     geoclue2.enable = true;
-    gnome.games.enable = true;
+    # gnome.games.enable = true;
   };
   # Fix broken stuff
-  services.avahi.enable = false;
-  networking.networkmanager.enable = false;
+  # services.avahi.enable = false;
+  networking.networkmanager.enable = true;
 
   environment.systemPackages = with pkgs;
     with gnome;
     with gnomeExtensions; [
+      feh
+      rofi
+
       gpaste
       gnome3.gnome-tweaks
 
