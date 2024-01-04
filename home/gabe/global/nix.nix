9624068f2031b4e32caf16b852b9c164c1c174d2
Gabe Dunn <gabe@sent.at>
2024-01-04 14:13:43 -0700

    add own cachix substituter

--- home/gabe/global/nix.nix
+++ home/gabe/global/nix.nix
@@ -10,11 +10,13 @@
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
         "https://devenv.cachix.org"
+        "https://gabedunn.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+        "gabedunn.cachix.org-1:wLWTKadNjpr2Op3rBnDZMUmUEPPIoKG87oY4PmBP8qU="
       ];
     };
 
