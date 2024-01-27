cf7db73a8ebe1ea0cec878ef5d76e2dd2c7d84e0
Gabe Dunn <gabe@sent.at>
2024-01-26 16:47:30 -0800

    remove gpg-agent stuff from ssh

--- home/gabe/features/cli/ssh.nix
+++ home/gabe/features/cli/ssh.nix
@@ -4,10 +4,6 @@ in {
   programs.ssh = let
     user = "gabe";
     identityFile = "~/.ssh/id_ed25519";
-    remoteForwards = [{
-      bind.address = "/%d/.gnupg-sockets/S.gpg-agent";
-      host.address = "/%d/.gnupg-sockets/S.gpg-agent.extra";
-    }];
   in {
     enable = true;
 
@@ -18,7 +14,6 @@ in {
         identityFile = identityFile;
         hostname = "${name}.colobus-pirate.ts.net";
         forwardAgent = true;
-        remoteForwards = remoteForwards;
       };
     in {
       "bastion" = mkDevice "bastion";
@@ -30,7 +25,6 @@ in {
         identityFile = identityFile;
         hostname = "titan.usbx.me";
         forwardAgent = true;
-        remoteForwards = remoteForwards;
       };
       "aur" = {
         user = "aur";
