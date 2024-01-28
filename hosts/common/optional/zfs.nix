5c2138f06ad4cfeb09df93f2c53ed7ef9aac2dbc
Gabe Dunn <gabe@sent.at>
2024-01-28 12:12:08 -0800

    set zfs mounts

--- hosts/common/optional/zfs.nix
+++ hosts/common/optional/zfs.nix
@@ -1,15 +1,8 @@
 { pkgs, lib, ... }:
 
 {
-  # boot.zfs = {
-  #   enabled = true;
-  #   extraPools = [ "pool" ];
-  #   # allowHibernation = true;
-  # };
-
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.forceImportRoot = false;
-  networking.hostId = "74996f49";
 
   services.zfs = {
     autoSnapshot.enable = true;
--- hosts/quasar/filesystem.nix
+++ hosts/quasar/filesystem.nix
@@ -1,5 +1,12 @@
+{ lib, ... }:
+
 {
-  fileSystems = {
+  fileSystems = let
+    mkZfs = name: {
+      device = name;
+      fsType = "zfs";
+    };
+  in {
     "/" = {
       device = "/dev/disk/by-uuid/7ed137e8-9837-49e9-95a0-fbd56358995f";
       fsType = "ext4";
@@ -9,7 +16,22 @@
       device = "/dev/disk/by-uuid/39F1-D337";
       fsType = "vfat";
     };
+
+    # zfs mounts
+    "/pool" = mkZfs "pool";
+    "/pool/downloads" = mkZfs "pool/downloads";
+    "/pool/data" = mkZfs "pool/data";
+    "/pool/settings" = mkZfs "pool/settings";
+    "/pool/media" = mkZfs "pool/media";
+    "/pool/cloud" = mkZfs "pool/cloud";
+
+    "/lake" = mkZfs "lake";
   };
+
+  boot.zfs.extraPools = [ "pool" "lake" ];
+
+  # needed for zfs
+  networking.hostId = "74996f49";
 
   swapDevices = [ ];
 }
