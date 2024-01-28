5d7baf8e847db4c9b58fef7f89c4fb1659d30399
Gabe Dunn <gabe@sent.at>
2024-01-28 14:26:53 -0800

    switch cockpit-zfs-manager to fork that supports nixos

--- pkgs/cockpit/zfs-manager/default.nix
+++ pkgs/cockpit/zfs-manager/default.nix
@@ -1,17 +1,15 @@
-{ lib, stdenv, fetchFromGitHub, zfs }:
+{ lib, stdenv, fetchFromGitHub }:
 
 stdenv.mkDerivation rec {
   pname = "cockpit-navigator";
   version = "1.3.1";
 
   src = fetchFromGitHub {
-    owner = "45Drives";
+    owner = "leroycep";
     repo = "cockpit-zfs-manager";
-    rev = "v${version}";
-    hash = "sha256-ge3wrri/eG1HprFSBYkjlqLzYOM3S4gUoqyE1w2Grz8=";
+    rev = "816af25099fccc46a3bff5f831b39d98ef33d514";
+    hash = "sha256-aLdsHHVVG6eJrvh3B4grZkDEbCZyDJjX2PuKkON8UcI=";
   };
-
-  buildInputs = [ zfs ];
 
   installPhase = ''
       mkdir -p $out/share/cockpit/zfs
@@ -21,7 +19,7 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "Cockpit UI for managing ZFS";
     license = licenses.lgpl3Only;
-    homepage = "https://github.com/45Drives/cockpit-zfs-manager";
+    homepage = "https://github.com/leroycep/cockpit-zfs-manager";
     platforms = platforms.linux;
     maintainers = with maintainers; [ ];
   };
