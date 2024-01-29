57cabb132594fb9d09f8939368f4d6bddf5e7625
Gabe Dunn <gabe@sent.at>
2024-01-29 00:51:31 -0800

    add startup command to disable screen blanking and remove duplicate mimeapps

--- home/gabe/features/desktop/bspwm/default.nix
+++ home/gabe/features/desktop/bspwm/default.nix
@@ -93,7 +93,8 @@
         startupPrograms = with pkgs;
           [
             "${config.home.homeDirectory}/.fehbg"
-            "${xorg.xset}/bin/xset r rate 240 40"
+            "${xorg.xset}/bin/xset r rate 240 40" # keyboard repeat rate
+            "${xorg.xset}/bin/xset s off -dpms" # disable screen blanking
             "${xorg.xsetroot}/bin/xsetroot -cursor_name left_ptr"
           ] ++ map runOnce [
             "${networkmanagerapplet}/bin/nm-applet --indicator"
@@ -111,54 +112,6 @@
             rule = "state=floating desktop='^7'";
           }];
       };
-  };
-
-  xdg.mimeApps = let
-    vivaldiDesktop = "vivaldi.desktop";
-    firefox = "firefox-developer-edition.desktop";
-  in {
-    enable = true;
-
-    associations.added = {
-      "image/jpeg" = [ "feh.desktop" "org.gnome.gThumb.desktop" ];
-      "image/png" = [ "feh.desktop" "org.gnome.gThumb.desktop" ];
-      "image/gif" = [ "feh.desktop" ];
-      "image/svg+xml" = [
-        "feh.desktop"
-        "nvim.desktop"
-        "sublime_text.desktop"
-        firefox
-        vivaldiDesktop
-      ];
-      "application/xml" = [ "nvim.desktop" "sublime_text.desktop" ];
-      "text/plain" = [ "nvim.desktop" "sublime_text.desktop" ];
-      "text/html" =
-        [ firefox vivaldiDesktop "nvim.desktop" "sublime_text.desktop" ];
-      "application/javascript" = [ "nvim.desktop" "sublime_text.desktop" ];
-      "application/json" = [
-        "org.gnome.TextEditor.desktop"
-        "sublime_text.desktop"
-        "nvim.desktop"
-      ];
-      "application/x-raw-disk-image" = [ "7zFM.desktop" ];
-      "application/octet-stream" = [ "nvim.desktop" "sublime_text.desktop" ];
-      "application/toml" = [ "nvim.desktop" "sublime_text.desktop" ];
-      "application/x-shellscript" =
-        [ "nvim.desktop" "kitty-open.desktop" "sublime_text.desktop" ];
-      "audio/ogg" = [ "mpv.desktop" "vlc.desktop" ];
-      "video/mp4" = [ "mpv.desktop" "vlc.desktop" ];
-    };
-    defaultApplications = {
-      "inode/directory" = [ "thunar.desktop" ];
-      "image/jpeg" = [ "feh.desktop" ];
-      "image/png" = [ "feh.desktop" ];
-      "text/plain" = [ "nvim.desktop" ];
-      "application/json" = [ "nvim.desktop" ];
-      "application/x-gnome-saved-search" = [ "thunar.desktop" ];
-      "video/mp4" = [ "mpv.desktop" ];
-      "x-scheme-handler/postman" = [ "Postman.desktop" ];
-      "x-scheme-handler/anytype" = [ "anytype.desktop" ];
-    };
   };
 
   # window resizing data for jgmenu
