51f0675879aed1e3fcdb366e1a761850197d4442
Gabe Dunn <gabe@sent.at>
2024-01-28 23:52:12 -0800

    fix bastion portainer version

--- hosts/bastion/services.nix
+++ hosts/bastion/services.nix
@@ -9,13 +9,26 @@
         image = "ghcr.io/redxtech/startpage";
         ports = [ "9009:3000" ];
       };
+
       portainer = {
-        image = "portainer/portainer-ce";
+        image = "portainer/portainer-ee";
         ports = [ "8000:8000" "9000:9000" ];
         volumes = [
           "/var/run/docker.sock:/var/run/docker.sock"
-          "portainer-data:/data"
+          "/var/run/portainer:/data"
         ];
+        extraOptions = [ "--network=host" ];
+      };
+
+      portainer-agent = {
+        image = "portainer/agent:2.19.4";
+        ports = [ "9001:9001" ];
+        volumes = [
+          "/var/lib/docker/volumes:/var/lib/docker/volumes"
+          "/var/run/docker.sock:/var/run/docker.sock"
+          "/:/host"
+        ];
+        extraOptions = [ "--network=host" ];
       };
     };
   };
