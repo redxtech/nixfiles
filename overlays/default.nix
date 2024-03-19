49c37713df5ff3bbe3bd30cef3c489770382191c
Gabe Dunn <gabe@sent.at>
2024-03-19 07:35:44 -0700

    remove unused overlay for flake input pkgs

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,18 +5,7 @@ let
     pkg.overrideAttrs
     (oldAttrs: { patches = (oldAttrs.patches or [ ]) ++ patches; });
 in {
-  # For every flake input, aliases 'pkgs.inputs.${flake}' to
-  # 'inputs.${flake}.packages.${pkgs.system}' or
-  # 'inputs.${flake}.legacyPackages.${pkgs.system}'
-  flake-inputs = final: _: {
-    inputs = builtins.mapAttrs (_: flake:
-      let
-        legacyPackages = ((flake.legacyPackages or { }).${final.system} or { });
-        packages = ((flake.packages or { }).${final.system} or { });
-      in if legacyPackages != { } then legacyPackages else packages) inputs;
-  };
-
-  # Adds my custom packages
+  # adds my custom packages
   additions = final: prev:
     import ../pkgs { pkgs = final; } // {
       # formats = prev.formats // import ../pkgs/formats { pkgs = final; };
