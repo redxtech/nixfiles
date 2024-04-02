bbae3db3262af40dcd94f12818df1dfed82785ae
Gabe Dunn <gabe@sent.at>
2024-04-01 23:59:34 -0700

    re-add cli to quasar home manager

--- home/gabe/quasar.nix
+++ home/gabe/quasar.nix
@@ -1,13 +1,11 @@
 { pkgs, lib, config, ... }:
 
 {
-  imports = [ ./global ./features/desktop/gnome ];
+  imports = [
+    ./global
 
-  # rename wireplumber devices
-  # xdg.configFile."wireplumber/main.lua.d/51-alsa-rename.lua".text = ''
-  #   table.insert(alsa_monitor.rules, {
-  #     matches = { { { "node.name", "matches", "alsa_output.usb-Schiit_Audio_Schiit_Unison_Modi_Multi_2-00.*" } } },
-  #     apply_properties = { ["node.description"] = "Schiit Stack" },
-  #   })
-  # '';
+    ./features/desktop/gnome
+  ];
+
+  cli.enable = true;
 }
