c89afd7f8397a3f73c8ddd088dbc2087e28fe5a2
Gabe Dunn <gabe@sent.at>
2024-05-14 00:32:17 -0600

    more settings renamed

--- modules/nixos/desktop/dm.nix
+++ modules/nixos/desktop/dm.nix
@@ -17,22 +17,15 @@ in {
   };
 
   config = mkIf cfg.enable {
-    services.xserver = {
-      displayManager = {
-        sddm = mkIf (cfg.dm == "sddm") {
-          enable = true;
-          theme = "catppuccin";
-          wayland.enable = true;
+    services.displayManager = {
+      sddm = mkIf (cfg.dm == "sddm") {
+        enable = true;
+        theme = "catppuccin";
+        wayland.enable = true;
 
-          settings = {
-            General = { CursorTheme = "Vimix-Cursors"; };
-            Theme = { EnableAvatars = true; };
-          };
-        };
-
-        gdm = mkIf (cfg.dm == "gdm") {
-          enable = true;
-          autoSuspend = false;
+        settings = {
+          General = { CursorTheme = "Vimix-Cursors"; };
+          Theme = { EnableAvatars = true; };
         };
       };
     };
