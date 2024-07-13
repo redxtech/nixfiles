c2eef94161fe47e2cdff818b2dbbf583cbfbd2de
Gabe Dunn <gabe@sent.at>
2024-07-13 03:59:30 -0600

    feat(deck): move filesystem off sd card lmao

--- hosts/deck/filesystem.nix
+++ hosts/deck/filesystem.nix
@@ -1,14 +1,14 @@
-{ pkgs, lib, ... }:
+{ config, pkgs, lib, ... }:
 
 {
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/21b496bf-a476-43fd-a24a-b79f6fcfda8a";
+    device = "/dev/disk/by-uuid/5ebc07cb-48a0-4cf4-b7d1-e0f4b6d8edd0";
     fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/BB20-581A";
+    device = "/dev/disk/by-uuid/7871-198B";
     fsType = "vfat";
     options = [ "fmask=0022" "dmask=0022" ];
   };
