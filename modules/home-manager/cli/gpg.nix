74b1546d8f894ecf161e74bdb32b4628875e28eb
Gabe Dunn <gabe@sent.at>
2024-08-08 16:04:49 -0600

    feat(home): improve gpg settings

--- modules/home-manager/cli/gpg.nix
+++ modules/home-manager/cli/gpg.nix
@@ -16,7 +16,7 @@ in {
       enable = false;
       enableSshSupport = true;
       sshKeys = [ "11148591F2B2026E9B2227BD5C7A1973A2838278" ];
-      pinentryPackage = if config.gtk.enable then
+      pinentryPackage = if config.desktop.enable then
         pkgs.pinentry-gnome3
       else
         pkgs.pinentry-curses;
@@ -25,7 +25,47 @@ in {
 
     programs.gpg = {
       enable = true;
-      settings = { trust-model = "tofu+pgp"; };
+      settings = {
+        personal-cipher-preferences = "AES256 AES192 AES";
+        personal-digest-preferences = "SHA512 SHA384 SHA256";
+        personal-compress-preferences = "ZLIB BZIP2 ZIP Uncompressed";
+        default-preference-list =
+          "SHA512 SHA384 SHA256 AES256 AES192 AES ZLIB BZIP2 ZIP Uncompressed";
+        cert-digest-algo = "SHA512";
+        s2k-digest-algo = "SHA512";
+        s2k-cipher-algo = "AES256";
+        charset = "utf-8";
+        no-comments = true;
+        no-emit-version = true;
+        no-greeting = true;
+        keyid-format = "0xlong";
+        list-options = "show-uid-validity";
+        verify-options = "show-uid-validity";
+        with-fingerprint = true;
+        # Display key origins and updates
+        #with-key-origin
+        require-cross-certification = true;
+        no-symkey-cache = true;
+        armor = true;
+        use-agent = true;
+        throw-keyids = true;
+        # Keyserver URLs
+        keyserver = [
+          "hkps://keys.openpgp.org"
+          "hkps://keyserver.ubuntu.com:443"
+          "hkps://pgpkeys.eu"
+          "hkps://pgp.circl.lu"
+        ];
+        # Enable key retrieval using WKD and DANE
+        auto-key-locate = "wkd,dane,local";
+        auto-key-retrieve = true;
+        trust-model = "tofu+pgp";
+        # Show expired subkeys
+        #list-options show-unusable-subkeys
+      };
+
+      scdaemonSettings.disable-ccid = true;
+
       publicKeys = [{
         source = ../../../home/gabe/keys/pgp.asc;
         trust = 5;
