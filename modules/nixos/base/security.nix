ba84cb6bc6ee4c479a40932e145f3b21346c5423
Gabe Dunn <gabe@gabedunn.dev>
2024-08-13 10:13:47 -0600

    feat(nixos/clamav): add clamtk to clam module, enable fangfrisch

--- modules/nixos/base/security.nix
+++ modules/nixos/base/security.nix
@@ -2,21 +2,20 @@
 
 let
   cfg = config.base;
-  inherit (lib) mkIf mkDefault;
+  inherit (lib) mkEnableOption mkIf mkDefault;
 in {
   options.base = {
     clamav = {
-      enable = lib.mkOption {
-        type = lib.types.bool;
-        default = true;
-        description = "Enable ClamAV antivirus";
-      };
+      enable = mkEnableOption "Enable ClamAV antivirus" // { default = true; };
+      fangfrisch = mkEnableOption "Enable fangfrisch" // { default = true; };
     };
   };
 
   config = mkIf cfg.enable {
-    # enable polkit
-    security.polkit.enable = true;
+    # install gui apps if desktop is enabled
+    environment.systemPackages = with pkgs;
+      mkIf config.desktop.enable [ clamtk ];
+
     # security.apparmor.enable = mkDefault true;
     # security.apparmor.killUnconfinedConfinables = mkDefault true;
 
@@ -26,46 +25,52 @@ in {
       daemon.enable = true;
       scanner.enable = true;
       updater.enable = true;
+      fangfrisch.enable = cfg.clamav.fangfrisch;
     };
 
-    # pwless sudo
-    security.sudo = {
-      enable = true;
+    security = {
+      # enable polkit
+      polkit.enable = true;
 
-      extraRules = let
-        mkRule = pkg: cmd: rules: [
-          {
-            command = "${pkg}/bin/${cmd}";
-            options = rules;
-          }
-          {
-            command = "/run/current-system/sw/bin/${cmd}";
-            options = rules;
-          }
-        ];
-        mkNoPwd = pkg: cmd: mkRule pkg cmd [ "NOPASSWD" ];
-      in [{
-        commands = (mkNoPwd pkgs.unixtools.fdisk "fdisk -l")
-          ++ (mkNoPwd pkgs.ps_mem "ps_mem")
-          ++ (mkNoPwd pkgs.systemd "systemctl restart xremap.service");
-        groups = [ "wheel" ];
-      }];
+      # pwless sudo
+      sudo = {
+        enable = true;
+
+        extraRules = let
+          mkRule = pkg: cmd: rules: [
+            {
+              command = "${pkg}/bin/${cmd}";
+              options = rules;
+            }
+            {
+              command = "/run/current-system/sw/bin/${cmd}";
+              options = rules;
+            }
+          ];
+          mkNoPwd = pkg: cmd: mkRule pkg cmd [ "NOPASSWD" ];
+        in [{
+          commands = (mkNoPwd pkgs.unixtools.fdisk "fdisk -l")
+            ++ (mkNoPwd pkgs.ps_mem "ps_mem")
+            ++ (mkNoPwd pkgs.systemd "systemctl restart xremap.service");
+          groups = [ "wheel" ];
+        }];
+      };
+
+      # increase open file limit for sudoers
+      pam.loginLimits = mkDefault [
+        {
+          domain = "@wheel";
+          item = "nofile";
+          type = "soft";
+          value = "524288";
+        }
+        {
+          domain = "@wheel";
+          item = "nofile";
+          type = "hard";
+          value = "1048576";
+        }
+      ];
     };
-
-    # increase open file limit for sudoers
-    security.pam.loginLimits = mkDefault [
-      {
-        domain = "@wheel";
-        item = "nofile";
-        type = "soft";
-        value = "524288";
-      }
-      {
-        domain = "@wheel";
-        item = "nofile";
-        type = "hard";
-        value = "1048576";
-      }
-    ];
   };
 }
