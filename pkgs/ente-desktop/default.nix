81a68377495e5fa372b9e47483e25db3075246d3
Gabe Dunn <gabe@gabedunn.dev>
2024-09-25 03:05:49 -0700

    chore: update ente-desktop

--- pkgs/ente-desktop/default.nix
+++ pkgs/ente-desktop/default.nix
@@ -1,15 +1,15 @@
-{ lib, fetchurl, appimageTools, imagemagick, ... }:
+{ lib, fetchurl, appimageTools, ... }:
 
 let
   pname = "ente-desktop";
-  version = "1.7.0";
+  version = "1.7.4";
   name = "${pname}-${version}";
 
   src = fetchurl {
     url =
       "https://github.com/ente-io/photos-desktop/releases/download/v${version}/ente-${version}-x86_64.AppImage";
     name = "${name}.AppImage";
-    hash = "sha256-utmUKTF+0+z3He/uKCiWG4BdZZxQDssgNvXGhyR/AMQ=";
+    hash = "sha256-6HXc0QXmgjbamrTI53u8SfbDPKnwr4TgOcuX+NBTQFY=";
   };
 
   appimageContents = appimageTools.extractType2 { inherit name src; };
@@ -24,19 +24,19 @@ in appimageTools.wrapType2 {
     ln -s $out/bin/${name} $out/bin/${pname}
 
     install -m 444 -D ${appimageContents}/ente.desktop $out/share/applications/ente.desktop
-
-    mkdir -p $out/share/icons/hicolor/512x512/apps
-    ${imagemagick}/bin/convert ${appimageContents}/usr/share/icons/hicolor/0x0/apps/ente.png -resize 512x512 $out/share/icons/hicolor/512x512/apps/ente.png
+    cp -r ${appimageContents}/usr/share/icons $out/share
 
     substituteInPlace $out/share/applications/ente.desktop \
       --replace-fail 'Exec=AppRun --no-sandbox' 'Exec=ente-desktop'
   '';
 
   meta = with lib; {
-    description = "";
-    homepage = "";
-    changelog = "";
-    license = licenses.mit;
+    description =
+      "The sweetness of Ente Photos, right on your computer. Linux, Windows and macOS.";
+    homepage = "https://github.com/ente-io/ente/tree/main/desktop";
+    changelog =
+      "https://github.com/ente-io/photos-desktop/releases/tag/v${version}";
+    license = licenses.agpl3Only;
     maintainers = with maintainers; [ redxtech ];
     platforms = [ "x86_64-linux" ];
   };
