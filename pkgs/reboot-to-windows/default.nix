ed53ac38b28ce842994f611e0b26930a1dd7d351
Gabe Dunn <gabe@gabedunn.dev>
2024-10-09 16:42:49 -0700

    refactor: clean up reboot-to-windows

--- pkgs/reboot-to-windows/default.nix
+++ pkgs/reboot-to-windows/default.nix
@@ -7,17 +7,13 @@ makeDesktopItem {
   desktopName = "Reboot to Windows";
   genericName = "reboot to windows";
   comment = "reboot to windows";
-  icon = let
-    icon = fetchurl {
-      url =
-        "https://raw.githubusercontent.com/Wartybix/Reboot-To-Windows/refs/heads/master/icons/reboot-to-windows.svg";
-      hash = "sha256-49+Q+C4N8v++U0NW6syVT86ypEbac9HfFRq1cp3+LEU=";
-    };
-  in icon;
+  icon = fetchurl {
+    url =
+      "https://raw.githubusercontent.com/Wartybix/Reboot-To-Windows/refs/heads/master/icons/reboot-to-windows.svg";
+    hash = "sha256-49+Q+C4N8v++U0NW6syVT86ypEbac9HfFRq1cp3+LEU=";
+  };
   categories = [ "System" ];
-  exec = let
-    reboot-to-windows = writeShellScriptBin "reboot-to-windows" ''
-      systemctl reboot --boot-loader-entry=${bootloader-entry} > /home/gabe/test.log 2>&1
-    '';
-  in lib.getExe reboot-to-windows;
+  exec = lib.getExe (writeShellScriptBin "reboot-to-windows" ''
+    systemctl reboot --boot-loader-entry=${bootloader-entry} > /home/gabe/test.log 2>&1
+  '');
 }
