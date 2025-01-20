bd1b5e856f2a99ef38045f38d528b1388be6677c
Gabe Dunn <gabe@gabedunn.dev>
2025-01-19 21:39:44 -0800

    feat(pkgs): add working moondeck-buddy

--- pkgs/moondeck-buddy/default.nix
+++ pkgs/moondeck-buddy/default.nix
@@ -1,40 +1,38 @@
-{ pkgs ? import <nixpkgs> { system = builtins.currentSystem; }, lib ? pkgs.lib
-, fetchurl ? pkgs.fetchurl, appimageTools ? pkgs.appimageTools }:
+{ lib, stdenv, fetchFromGitHub, pkg-config, wrapQtAppsHook, qtbase, cmake, qt6
+, procps, xorg }:
 
 let
   pname = "moondeck-buddy";
-  version = "1.6.1";
-  name = "${pname}-${version}";
+  version = "1.6.3";
 
-  src = fetchurl {
-    url =
-      "https://github.com/FrogTheFrog/${pname}/releases/download/v${version}/MoonDeckBuddy-${version}-x86_64.AppImage";
-    hash = "sha256-FteDRbjv4YTuLkdUxSrusXgNm0xpuqN+EAvjSGAq4rY=";
+  qtEnv = with qt6;
+    env "qt-env-custom-moondeck-buddy-${qtbase.version}" [
+      qthttpserver
+      qtwebsockets
+    ];
+in stdenv.mkDerivation {
+  inherit pname version;
+
+  src = fetchFromGitHub {
+    owner = "FrogTheFrog";
+    repo = pname;
+    tag = "v${version}";
+    hash = "sha256-U5Q/NQitOVTgaGtfckss7Bpn9hvIRFMSAZ9Uh+hVIic=";
   };
 
-  appimageContents = appimageTools.extractType2 { inherit pname version src; };
-in appimageTools.wrapType2 {
-  inherit pname version src;
+  buildInputs = [ cmake procps xorg.libXrandr qtbase qtEnv ];
+  nativeBuildInputs = [ pkg-config wrapQtAppsHook ];
 
-  multiArch = false; # no 32bit needed
-  extraPkgs = pkgs:
-    appimageTools.defaultFhsEnvArgs.multiPkgs pkgs ++ [ pkgs.bash pkgs.steam ];
-
-  extraInstallCommands = ''
-    install -m 555 -D ${appimageContents}/usr/bin/MoonDeckStream $out/bin/moondeck-stream-${version}
-
-    ln -s $out/bin/moondeck-stream-${version} $out/bin/moondeck-stream
-
-    install -m 444 -D ${appimageContents}/MoonDeckBuddy.desktop $out/share/applications/${pname}.desktop
-
-    for size in 16 32 48 64 128 256; do
-      install -m 444 -D ${appimageContents}/app.png \
-        $out/share/icons/hicolor/''${size}x''${size}/apps/${pname}.png
-    done
-
-    substituteInPlace $out/share/applications/${pname}.desktop \
-      --replace-fail 'Icon=app' 'Icon=${pname}' \
-      --replace-fail 'Exec=MoonDeckBuddy' 'Exec=${pname}'
+  preConfigure = let
+    ssl-deps = fetchFromGitHub {
+      owner = "FrogTheFrog";
+      repo = "moondeck-keys";
+      rev = "9d1cc7356181f6f6c0aa2a5e92ad7174f09fa539";
+      hash = "sha256-nvGNjxK878tx5fUGt8ACYeVcqs6KbKATXlwMe6ghS0A=";
+    };
+  in ''
+    mkdir -p resources/ssl
+    cp ${ssl-deps}/*.pem resources/ssl
   '';
 
   meta = with lib; {
