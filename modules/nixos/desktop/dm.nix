f27441e31cfa28a0f9302fd3bb05f32e128d8c52
Gabe Dunn <gabe@gabedunn.dev>
2025-05-05 17:19:01 -0600

    feat: use tuigreet as greetd greeter

--- modules/nixos/desktop/dm.nix
+++ modules/nixos/desktop/dm.nix
@@ -18,49 +18,11 @@ in {
   };
 
   config = mkIf cfg.enable {
-    programs.regreet = {
-      enable = cfg.dm == "greetd";
-
-      theme = {
-        name = "Dracula";
-        package = pkgs.dracula-theme;
-      };
-
-      iconTheme = {
-        name = "Papirus-Dark";
-        package = pkgs.papirus-icon-theme;
-      };
-
-      cursorTheme = {
-        name = "Vimix-cursors";
-        package = pkgs.vimix-cursors;
-      };
-
-      settings = {
-        GTK = {
-          application_prefer_dark_theme = true;
-          theme_name = "Dracula";
-        };
-
-        background = {
-          fit = "Cover";
-          path = let
-            bg = pkgs.fetchurl {
-              name = "all-i-need-4k.png";
-              url =
-                "https://slink.super.fish/image/4885fd3a-f8a8-4a81-9d4d-74cf04521b0c.png";
-              hash = "sha256-+4JR/SQ9yKLiiwEpe9arfnuvyVOq0xyLIGioaso6gIU=";
-            };
-          in "${bg}";
-        };
-      };
-    };
-
     services = {
       greetd = {
-        enable = true;
-        # for tuigreet, disable regreet to use
-        settings = mkIf (!config.programs.regreet.enable) {
+        enable = cfg.dm == "greetd";
+
+        settings = {
           default_session = {
             command = let
               hyprland =
