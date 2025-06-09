86b1d8b23fc837615d3731f22208485393b1e379
Gabe Dunn <gabe@gabedunn.dev>
2025-06-09 10:53:36 -0600

    feat: disable garnix & devenv binary caches

--- modules/flake/nix.nix
+++ modules/flake/nix.nix
@@ -10,18 +10,18 @@
           substituters = [
             "https://cache.nixos.org"
             "https://nix-community.cachix.org"
-            "https://cache.garnix.io"
-            "https://devenv.cachix.org"
             "https://gabedunn.cachix.org"
             "https://hyprland.cachix.org"
+            # "https://cache.garnix.io"
+            # "https://devenv.cachix.org"
           ];
           trusted-public-keys = [
             "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-            "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-            "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
             "gabedunn.cachix.org-1:wLWTKadNjpr2Op3rBnDZMUmUEPPIoKG87oY4PmBP8qU="
             "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+            # "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+            # "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
           ];
           auto-optimise-store = lib.mkDefault true;
           warn-dirty = false;
