089767b5e05f635e6a13ac83008254d4e369e64f
Gabe Dunn <gabe@gabedunn.dev>
2025-12-07 13:26:36 -0800

    feat: add nix-ld libraries

--- modules/nixos/base/cli.nix
+++ modules/nixos/base/cli.nix
@@ -28,8 +28,65 @@ in {
         };
       };
 
-      # for fugit2
-      nix-ld.libraries = with pkgs; [ libgit2 ];
+      nix-ld.libraries = with pkgs;
+        [
+          acl
+          attr
+          bzip2
+          dbus
+          expat
+          fontconfig
+          freetype
+          fuse3
+          icu
+          libgit2
+          libnotify
+          libsodium
+          libssh
+          libunwind
+          libusb1
+          libuuid
+          nspr
+          nss
+          stdenv.cc.cc
+          util-linux
+          zlib
+          zstd
+        ] ++ lib.optionals (config.hardware.graphics.enable) [
+          pipewire
+          cups
+          libxkbcommon
+          pango
+          mesa
+          libdrm
+          libglvnd
+          libpulseaudio
+          atk
+          cairo
+          alsa-lib
+          at-spi2-atk
+          at-spi2-core
+          gdk-pixbuf
+          glib
+          gtk3
+          libGL
+          libappindicator-gtk3
+          vulkan-loader
+          xorg.libX11
+          xorg.libXScrnSaver
+          xorg.libXcomposite
+          xorg.libXcursor
+          xorg.libXdamage
+          xorg.libXext
+          xorg.libXfixes
+          xorg.libXi
+          xorg.libXrandr
+          xorg.libXrender
+          xorg.libXtst
+          xorg.libxcb
+          xorg.libxkbfile
+          xorg.libxshmfence
+        ];
     };
 
     environment = {
