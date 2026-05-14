fe6b5f2c2c526939a6da59546f1b4d6483899eec
Gabe Dunn <gabe@sent.at>
2026-05-13 20:35:10 -0600

    feat: prevent discord from changing mic input volume

--- modules/features/workstation/apps/discord.nix
+++ modules/features/workstation/apps/discord.nix
@@ -1,59 +1,84 @@
 { inputs, ... }:
 
 {
-  den.aspects.discord.homeManager =
-    {
-      config,
-      pkgs,
-      lib,
-      ...
-    }:
-    {
-      imports = [ inputs.nixcord.homeModules.nixcord ];
-
-      programs.nixcord = {
-        enable = true;
-        discord.enable = false; # don't use official discord client
-
-        # native equicord client
-        equibop = {
-          enable = true;
-          settings = {
-            discordBranch = "stable";
-            tray = true;
-            minimizeToTray = true;
-            arRPC = true;
-            trayColor = "";
-            trayMainOverride = false;
-            splashColor = "rgb(239, 239, 241)";
+  den.aspects.discord = {
+    # block discord from changing mic input volume
+    nixos.services.pipewire.extraConfig.pipewire-pulse."10-disable-discord-volume-control"."pulse.rules" =
+      [
+        # rule for equibop & vesktop
+        {
+          matches = [
+            { "application.process.binary" = "electron"; }
+          ];
+          actions = {
+            quirks = [ "block-source-volume" ];
           };
-        };
-
-        # lightweight client
-        legcord = {
-          enable = true;
-          equicord.enable = true;
-          vencord.enable = false;
-
-          settings = {
-            hardwareAcceleration = true;
-            minimizeToTray = true;
-            tray = "dynamic";
+        }
+        # rule for official discord client
+        {
+          matches = [
+            { "application.process.binary" = ".Discord-wrapped"; }
+          ];
+          actions = {
+            quirks = [ "block-source-volume" ];
           };
-        };
-      };
-
-      # disable generating settings file until i make my own
-      home.file."${config.programs.nixcord.equibop.configDir}/settings/settings.json".enable =
-        lib.mkForce false;
-
-      xdg.autostart.entries = [
-        "${config.programs.nixcord.equibop.package}/share/applications/equibop.desktop"
+        }
       ];
 
-      # disable until i find a good theme
-      stylix.targets.nixcord.enable = false;
-    };
+    homeManager =
+      {
+        config,
+        pkgs,
+        lib,
+        ...
+      }:
+      {
+        imports = [ inputs.nixcord.homeModules.nixcord ];
+
+        programs.nixcord = {
+          enable = true;
+          discord.enable = false; # don't use official discord client
+
+          # native equicord client
+          equibop = {
+            enable = true;
+            settings = {
+              discordBranch = "stable";
+              tray = true;
+              minimizeToTray = true;
+              arRPC = true;
+              trayColor = "";
+              trayMainOverride = false;
+              splashColor = "rgb(239, 239, 241)";
+            };
+          };
+
+          # lightweight client
+          legcord = {
+            enable = true;
+            equicord.enable = true;
+            vencord.enable = false;
+
+            settings = {
+              hardwareAcceleration = true;
+              minimizeToTray = true;
+              tray = "dynamic";
+            };
+          };
+        };
+
+        # disable generating settings file until i make my own
+        home.file."${config.programs.nixcord.equibop.configDir}/settings/settings.json".enable =
+          lib.mkForce false;
+
+        xdg.autostart.entries = [
+          "${config.programs.nixcord.equibop.package}/share/applications/equibop.desktop"
+        ];
+
+        # disable until i find a good theme
+        stylix.targets.nixcord.enable = false;
+      };
+  };
 
   flake-file.inputs.nixcord = {
     url = "github:FlameFlag/nixcord";
