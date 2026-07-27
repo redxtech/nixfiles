00819c3bc3965e786998ebf5bccc34bdfdce4dbe
Gabe Dunn <gabe@gabedunn.dev>
2026-07-27 04:08:37 -0600

    feat: package openportal

---
+++ packages/openportal/package.nix
@@ -0,0 +1,49 @@
+{
+  perSystem =
+    { pkgs, ... }:
+    let
+      inherit (pkgs)
+        bun
+        fetchurl
+        lib
+        makeWrapper
+        stdenvNoCC
+        ;
+
+      version = "0.1.32";
+
+      src = fetchurl {
+        url = "https://registry.npmjs.org/openportal/-/openportal-${version}.tgz";
+        hash = "sha256-T/or2oyGwvKLaGSnY+gNVUY4oxcAsnd7Lm1qJ4ZgJCs=";
+      };
+    in
+    {
+      packages.openportal = stdenvNoCC.mkDerivation (finalAttrs: {
+        pname = "openportal";
+        inherit version src;
+
+        nativeBuildInputs = [ makeWrapper ];
+
+        installPhase = ''
+          runHook preInstall
+
+          mkdir -p $out/lib/openportal $out/bin
+          cp -r dist web $out/lib/openportal/
+          makeWrapper ${lib.getExe bun} $out/bin/openportal \
+            --add-flags $out/lib/openportal/dist/index.js \
+            --prefix PATH : ${lib.makeBinPath [ bun ]}
+
+          runHook postInstall
+        '';
+
+        meta = {
+          description = "Mobile-first web interface for coding agents";
+          homepage = "https://github.com/hosenur/portal";
+          changelog = "https://github.com/hosenur/portal/releases/tag/cli-v${finalAttrs.version}";
+          license = lib.licenses.mit;
+          mainProgram = "openportal";
+          platforms = lib.platforms.linux;
+        };
+      });
+    };
+}
