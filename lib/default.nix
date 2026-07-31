352d085ca8e492ef214132ebf7d662db68b1963c
Gabe Dunn <gabe@gabedunn.dev>
2026-07-31 12:33:37 -0600

    chore: remove migrated quasar oldflake configuration

--- lib/default.nix
+++ lib/default.nix
@@ -7,9 +7,6 @@ in
   # functions to help with containers
   containers = import ./containers args;
 
-  # nas helpers
-  nas = import ./nas args;
-
   # server service helpers
   server = import ./server args;
 
--- lib/nas/paths.nix
+++
@@ -1,9 +0,0 @@
-{ lib, ... }:
-
-paths: with paths; {
-  mkConf = name: config + "/" + name + ":/config";
-  mkData = name: data + "/" + name + ":/data";
-  mkDl = name: downloads + "/" + name + ":/downloads";
-  downloads = downloads + ":/downloads";
-  media = media + ":/media";
-}
