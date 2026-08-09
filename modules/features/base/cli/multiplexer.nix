376a7729bfed105ed3f974a5ff0bb86267991e77
Gabe Dunn <gabe@gabedunn.dev>
2026-08-09 11:11:57 -0600

    feat: remove tmux-continuum

--- modules/features/base/cli/multiplexer.nix
+++ modules/features/base/cli/multiplexer.nix
@@ -42,13 +42,6 @@
             plugin = resurrect;
             extraConfig = "set -g @resurrect-strategy-nvim 'session'";
           }
-          {
-            plugin = continuum;
-            extraConfig = ''
-              set -g @continuum-restore 'on'
-              set -g @continuum-save-interval '60' # minutes
-            '';
-          }
         ];
 
         extraConfig = ''
