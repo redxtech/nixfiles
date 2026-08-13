5576f693f5599351d55755d0b99ab211bcbf204b
Gabe Dunn <gabe@gabedunn.dev>
2026-08-13 09:19:17 -0600

    feat: update cockpit-machines to 355
    Use the upstream source build with vendored dependencies, update metadata, and add an automatic GitHub release update script.

--- packages/cockpit/machines/package.nix
+++ packages/cockpit/machines/package.nix
@@ -3,48 +3,76 @@
     {
       pkgs,
       lib,
+      packageUpdateScripts,
       ...
     }:
     {
       packages.cockpit-machines =
         let
           inherit (pkgs)
-            stdenv
-            fetchzip
+            cockpit
+            fetchFromGitHub
             gettext
+            libosinfo
+            nodejs
+            osinfo-db
+            stdenv
+            writeShellScriptBin
             ;
-
-          pname = "cockpit-machines";
-          # later releases require the full source-build recipe used by nixpkgs.
-          version = "341";
         in
-        stdenv.mkDerivation {
-          inherit pname version;
+        stdenv.mkDerivation (finalAttrs: {
+          pname = "cockpit-machines";
+          version = "355";
 
-          src = fetchzip {
-            url = "https://github.com/cockpit-project/cockpit-machines/releases/download/${version}/cockpit-machines-${version}.tar.xz";
-            hash = "sha256-Tsv18wAN02zQEerIeHAvfs5e0cIWfi7nQey8n1Hv6HI=";
+          src = fetchFromGitHub {
+            owner = "cockpit-project";
+            repo = "cockpit-machines";
+            tag = finalAttrs.version;
+            hash = "sha256-9NRpo9j0EDTWvJAPuAhr3H6Gigt7MdwYbdXZeGqHVGo=";
+
+            fetchSubmodules = true;
+            postFetch = "cp $out/node_modules/.package-lock.json $out/package-lock.json";
           };
 
-          nativeBuildInputs = [ gettext ];
+          buildInputs = [
+            nodejs
+            gettext
+            (writeShellScriptBin "git" "true")
+          ];
 
-          makeFlags = [ "PREFIX=$(out)" ];
+          cockpitSrc = cockpit.src;
 
           postPatch = ''
-            touch pkg/lib/cockpit.js
-            touch pkg/lib/cockpit-po-plugin.js
-            touch dist/manifest.json
+            mkdir -p pkg; cp -r $cockpitSrc/pkg/lib pkg
+            mkdir -p test; cp -r $cockpitSrc/test/common test
+
+            substituteInPlace Makefile \
+              --replace-fail '$(MAKE) package-lock.json' 'true' \
+              --replace-fail '$(COCKPIT_REPO_FILES) | tar x' "" \
+              --replace-fail '/usr/local' "$out"
+
+            substituteInPlace src/manifest.json \
+              --replace-fail '"/usr/share/dbus-1/system.d/org.libvirt.conf"' '"/etc/systemd/system/libvirt-dbus.service"'
+
+            patchShebangs build.js
           '';
 
-          dontBuild = true;
+          passthru = {
+            updateScript = packageUpdateScripts.githubRelease;
+            cockpitPath = [
+              libosinfo
+              osinfo-db
+            ];
+          };
 
           meta = with lib; {
             description = "Cockpit UI for virtual machines";
-            license = licenses.lgpl21;
             homepage = "https://github.com/cockpit-project/cockpit-machines";
+            changelog = "https://github.com/cockpit-project/cockpit-machines/releases/tag/${finalAttrs.version}";
             platforms = platforms.linux;
+            license = licenses.lgpl21;
             maintainers = with maintainers; [ redxtech ];
           };
-        };
+        });
     };
 }
