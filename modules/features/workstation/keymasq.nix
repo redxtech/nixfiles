6f12a4f1caab55349270975da8c6b85a0f9d820e
Gabe Dunn <gabe@gabedunn.dev>
2026-09-22 03:13:40 -0600

    feat: add symlinks to include keymasq config and still use gui

--- modules/features/workstation/keymasq.nix
+++ modules/features/workstation/keymasq.nix
@@ -12,5 +12,21 @@
       };
     };
 
+  den.aspects.keymasq.homeManager =
+    { config, ... }:
+    let
+      configRoot = "${config.home.homeDirectory}/Code/nixfiles/modules/features/workstation/keymasq";
+      link = path: config.lib.file.mkOutOfStoreSymlink "${configRoot}/${path}";
+    in
+    {
+      xdg.configFile = {
+        "keymasq/profiles".source = link "shared/profiles";
+        "keymasq/superkeys".source = link "shared/superkeys";
+        "keymasq/analog_controls".source = link "shared/analog_controls";
+        "keymasq/motion_controls".source = link "shared/motion_controls";
+        "keymasq/hardware".source = link "shared/hardware";
+      };
+    };
+
   flake-file.inputs.keymasq.url = "github:nyrda/keymasq";
 }
